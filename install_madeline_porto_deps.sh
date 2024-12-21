da18338491cc22b1d07c3f956a8475211634126a
guloc <109765419+guloc@users.noreply.github.com>
2024-12-21 17:10:22 +0300

    Update install_madeline_porto_deps.sh
    .

--- install_madeline_porto_deps.sh
+++ install_madeline_porto_deps.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
+# Функция для вывода сообщений
+log_message() {
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
+}
+
 # Функция для автоматического ответа на запросы apt
 apt_noninteractive() {
     DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" "$@"
 }
 
+log_message "Начало выполнения скрипта"
+
 # Обновление списка пакетов
+log_message "Обновление списка пакетов"
 apt_noninteractive update
 
 # Добавление репозитория PHP
+log_message "Добавление репозитория PHP"
 add-apt-repository ppa:ondrej/php -y
 apt_noninteractive update
 
 # Удаление старых версий PHP
-apt_noninteractive remove php7.4 php7.4-* php8.0 php8.0-* php8.1 php8.1-*
+log_message "Удаление старых версий PHP"
+apt_noninteractive remove php7.4 php7.4-* php8.0 php8.0-* php8.1 php8.1-* php8.4 php8.4-*
 apt_noninteractive autoremove
 
 # Установка PHP 8.3 и необходимых расширений
-apt_noninteractive install php8.3 php8.3-cli php8.3-common php8.3-curl php8.3-mbstring php8.3-mysql php8.3-xml php8.3-zip php8.3-gd php8.3-bcmath php8.3-intl php8.3-readline php8.3-ldap php8.3-tidy php8.3-soap php8.3-igbinary php8.3-memcached php8.3-redis php8.3-gmp
+log_message "Установка PHP 8.3 и необходимых расширений"
+apt_noninteractive install php8.3 php8.3-fpm php8.3-cli php8.3-common php8.3-curl php8.3-mbstring php8.3-mysql php8.3-xml php8.3-zip php8.3-gd php8.3-bcmath php8.3-intl php8.3-readline php8.3-ldap php8.3-tidy php8.3-soap php8.3-igbinary php8.3-memcached php8.3-redis php8.3-gmp
 
 # Установка ioncube loader для PHP 8.3
+log_message "Установка ioncube loader для PHP 8.3"
 wget https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
 tar xzf ioncube_loaders_lin_x86-64.tar.gz
 PHP_VERSION="8.3"
@@ -30,22 +42,36 @@ ln -s /etc/php/${PHP_VERSION}/mods-available/ioncube.ini /etc/php/${PHP_VERSION}
 ln -s /etc/php/${PHP_VERSION}/mods-available/ioncube.ini /etc/php/${PHP_VERSION}/fpm/conf.d/00-ioncube.ini
 
 # Установка ffmpeg
-apt install -y ffmpeg
+log_message "Установка ffmpeg"
+apt_noninteractive install ffmpeg
 
 # Очистка
+log_message "Очистка временных файлов"
 rm -rf ioncube*
 
 # Настройка Apache для использования PHP 8.3 по умолчанию
-a2dismod php7.4 php8.0 php8.1
+log_message "Настройка Apache для использования PHP 8.3"
+a2dismod php7.4 php8.0 php8.1 php8.4
 a2enmod php8.3
 
 # Перезапуск Apache и PHP-FPM
+log_message "Перезапуск Apache и PHP-FPM"
 systemctl restart apache2
 systemctl restart php8.3-fpm
 
-# Удаление конфигураций старых версий PHP из Fast Panel
-rm -f /etc/nginx/fastpanel2/php_versions/php7.4.conf
-rm -f /etc/nginx/fastpanel2/php_versions/php8.0.conf
-rm -f /etc/nginx/fastpanel2/php_versions/php8.1.conf
+# Обновление конфигураций Fast Panel
+log_message "Обновление конфигураций Fast Panel"
+sed -i 's/php7.4/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
+sed -i 's/php8.0/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
+sed -i 's/php8.1/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
+sed -i 's/php8.4/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
 
-echo "Установка завершена. PHP версии 8.3 установлен с необходимыми расширениями, ioncube loader и ffmpeg. Старые версии PHP удалены."
+# Перезапуск Nginx
+log_message "Перезапуск Nginx"
+systemctl restart nginx
+
+# Проверка установленной версии PHP
+INSTALLED_PHP_VERSION=$(php -r "echo PHP_VERSION;")
+log_message "Установленная версия PHP: $INSTALLED_PHP_VERSION"
+
+echo "Установка завершена. PHP версии $INSTALLED_PHP_VERSION установлен с необходимыми расширениями, ioncube loader и ffmpeg. Старые версии PHP удалены."
