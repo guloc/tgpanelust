9090211858e9f510199380dfa6814122e709711d
guloc <109765419+guloc@users.noreply.github.com>
2024-12-21 17:16:20 +0300

    Update install_madeline_porto_deps.sh
    .

--- install_madeline_porto_deps.sh
+++ install_madeline_porto_deps.sh
@@ -21,11 +21,6 @@ log_message "Добавление репозитория PHP"
 add-apt-repository ppa:ondrej/php -y
 apt_noninteractive update
 
-# Удаление старых версий PHP
-log_message "Удаление старых версий PHP"
-apt_noninteractive remove php7.4 php7.4-* php8.0 php8.0-* php8.1 php8.1-* php8.4 php8.4-*
-apt_noninteractive autoremove
-
 # Установка PHP 8.3 и необходимых расширений
 log_message "Установка PHP 8.3 и необходимых расширений"
 apt_noninteractive install php8.3 php8.3-fpm php8.3-cli php8.3-common php8.3-curl php8.3-mbstring php8.3-mysql php8.3-xml php8.3-zip php8.3-gd php8.3-bcmath php8.3-intl php8.3-readline php8.3-ldap php8.3-tidy php8.3-soap php8.3-igbinary php8.3-memcached php8.3-redis php8.3-gmp
@@ -51,8 +46,9 @@ rm -rf ioncube*
 
 # Настройка Apache для использования PHP 8.3 по умолчанию
 log_message "Настройка Apache для использования PHP 8.3"
-a2dismod php7.4 php8.0 php8.1 php8.4
 a2enmod php8.3
+a2dismod php7.4 php8.0 php8.1 php8.2 php8.4
+update-alternatives --set php /usr/bin/php8.3
 
 # Перезапуск Apache и PHP-FPM
 log_message "Перезапуск Apache и PHP-FPM"
@@ -61,10 +57,13 @@ systemctl restart php8.3-fpm
 
 # Обновление конфигураций Fast Panel
 log_message "Обновление конфигураций Fast Panel"
-sed -i 's/php7.4/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
-sed -i 's/php8.0/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
-sed -i 's/php8.1/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
-sed -i 's/php8.4/php8.3/g' /etc/nginx/fastpanel2/php_versions.conf
+if [ -f "/etc/nginx/fastpanel2/php_versions.conf" ]; then
+    if ! grep -q "php8.3" /etc/nginx/fastpanel2/php_versions.conf; then
+        echo "php8.3 = /run/php/php8.3-fpm.sock;" >> /etc/nginx/fastpanel2/php_versions.conf
+    fi
+else
+    log_message "Файл php_versions.conf не найден. Пропуск обновления Fast Panel."
+fi
 
 # Перезапуск Nginx
 log_message "Перезапуск Nginx"
@@ -72,6 +71,6 @@ systemctl restart nginx
 
 # Проверка установленной версии PHP
 INSTALLED_PHP_VERSION=$(php -r "echo PHP_VERSION;")
-log_message "Установленная версия PHP: $INSTALLED_PHP_VERSION"
+log_message "Установленная версия PHP по умолчанию: $INSTALLED_PHP_VERSION"
 
-echo "Установка завершена. PHP версии $INSTALLED_PHP_VERSION установлен с необходимыми расширениями, ioncube loader и ffmpeg. Старые версии PHP удалены."
+echo "Установка завершена. PHP версии 8.3 установлен с необходимыми расширениями, ioncube loader и ffmpeg. PHP 8.3 настроен как версия по умолчанию."
